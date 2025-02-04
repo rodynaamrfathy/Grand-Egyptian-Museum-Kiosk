7e3efafd568755f4740cba9b4d6c60265f40cb6b
rodynaamrfathy <rodynaamr@icloud.com>
2025-02-04 14:37:52 +0200

    updated qrcode url

--- kiosk-ui/app/components/QRCodeDisplay.tsx
+++ kiosk-ui/app/components/QRCodeDisplay.tsx
@@ -3,7 +3,7 @@ const serverIp = process.env.NEXT_PUBLIC_SERVER_IP;
 import { QRCodeSVG } from "qrcode.react";
 
 const QRCodeDisplay = ({ imageUrl }: { imageUrl: string }) => {
-  const qrCodeUrl = `http://${serverIp}:3002/viewmedia?image=${encodeURIComponent(imageUrl)}`;
+  const qrCodeUrl = `http://${serverIp}:3002?image=${encodeURIComponent(imageUrl)}`;
 
   return (
     <div className="bg-white absolute scale-75 z-30 p-2 rounded-lg shadow-lg flex flex-col items-center transition-all duration-1000 opacity-100 bottom-8">
