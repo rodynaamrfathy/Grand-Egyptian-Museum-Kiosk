57421bde26986afe3d3981faac34ced5097faceb
rodynaamrfathy <rodynaamr@icloud.com>
2025-04-27 22:04:38 +0300

    talk to ramses page done

--- gem-webapp-ui/app/talk-to-ramses/page.tsx
+++ gem-webapp-ui/app/talk-to-ramses/page.tsx
@@ -1,25 +1,20 @@
 import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function TalkToRamses() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-900">
+    <div className="flex flex-col min-h-screen relative">
       {/* Header */}
-      <header className="bg-gray-500 text-white text-center p-5 w-full">
-        <h1 className="text-xl font-bold sm:text-2xl">Talk to Ramses</h1>
-      </header>
+      <Header />
 
       {/* Main Content */}
-      <main className="flex-1 p-5 flex flex-col items-center w-full sm:w-3/4 md:w-1/2">
-        <h2 className="text-lg font-bold mb-2 text-center">Welcome to Ramses!</h2>
-        <p className="text-center mb-4">Ask Ramses anything about Ancient Egypt.</p>
-
-        {/* Chat interface or content can go here */}
+      <main className="flex-1 bg-[url('/images/light_mode_background.svg')] dark:bg-[url('/images/dark_mode_background.svg')] bg-cover bg-center">
+      
       </main>
-
+      
       {/* Footer */}
-      <footer className="text-center p-5 bg-gray-800 text-white w-full">
-        <p>&copy; 2024 المتحف المصري الكبير. All Rights Reserved.</p>
-      </footer>
+      <Footer />
     </div>
   );
 }
